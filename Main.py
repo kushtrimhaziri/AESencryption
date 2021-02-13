4d6260d14f1e96b2e7137e483e8fa0f290ee9fdd
visarahmeti <58608274+Visskey@users.noreply.github.com>
2021-02-13 15:41:29 +0100

    Add the constructor of the class
    Adding all of the elements in the UI

--- Main.py
+++ Main.py
@@ -11,3 +11,214 @@ from EncryptionTool import EncryptionTool
 
 
 class Main:
+  
+    def __init__(self, root):
+        self.root = root
+        self._cipher = None
+        self._file_url = tk.StringVar()
+        self._secret_key = tk.StringVar()
+        self._salt = tk.StringVar()
+        self._status = tk.StringVar()
+        self._status.set("---")
+
+        self.should_cancel = False
+
+        root.title("AES Encryption")
+        root.configure(bg="#eeeeee")
+
+
+        self.menu_bar = tk.Menu(
+            root,
+            bg="#010101",
+            relief=tk.RAISED
+        )
+        self.menu_bar.add_command(
+            label="How To",
+            command=self.show_help_callback
+        )
+        self.menu_bar.add_command(
+            label="Exit!",
+            command=root.quit
+        )
+
+        root.configure(
+            menu=self.menu_bar
+        )
+
+        self.file_entry_label = tk.Label(
+            root,
+            text="Shtypni direktoriumin e filet qe doni te enkriptoni ose klikoni ne butonin Select File",
+            bg="#eeeeee",
+            anchor=tk.W
+        )
+        self.file_entry_label.grid(
+            padx=12,
+            pady=(8, 0),
+            ipadx=0,
+            ipady=1,
+            row=0,
+            column=0,
+            columnspan=4,
+            sticky=tk.W + tk.E + tk.N + tk.S
+        )
+
+        self.file_entry = tk.Entry(
+            root,
+            textvariable=self._file_url,
+            bg="#fff",
+            exportselection=0,
+            relief=tk.FLAT
+        )
+        self.file_entry.grid(
+            padx=15,
+            pady=6,
+            ipadx=8,
+            ipady=8,
+            row=1,
+            column=0,
+            columnspan=4,
+            sticky=tk.W + tk.E + tk.N + tk.S
+        )
+
+        self.select_btn = tk.Button(
+            root,
+            text="ZGJEDH FILEN",
+            command=self.selectfile_callback,
+            width=42,
+            bg="#010101",
+            fg="#ffffff",
+            bd=2,
+            relief=tk.FLAT
+        )
+
+        self.select_btn.grid(
+            padx=15,
+            pady=8,
+            ipadx=24,
+            ipady=6,
+            row=2,
+            column=0,
+            columnspan=4,
+            sticky=tk.W + tk.E + tk.N + tk.S
+        )
+
+        self.key_entry_label = tk.Label(
+            root,
+            text="Shtypni celesin tuaj sekret",
+            bg="#eeeeee",
+            anchor=tk.W
+        )
+        self.key_entry_label.grid(
+            padx=12,
+            pady=(8, 0),
+            ipadx=0,
+            ipady=1,
+            row=3,
+            column=0,
+            columnspan=4,
+            sticky=tk.W + tk.E + tk.N + tk.S
+        )
+
+        self.key_entry = tk.Entry(
+            root,
+            textvariable=self._secret_key,
+            bg="#fff",
+            exportselection=0,
+            relief=tk.FLAT
+        )
+        self.key_entry.grid(
+            padx=15,
+            pady=6,
+            ipadx=8,
+            ipady=8,
+            row=4,
+            column=0,
+            columnspan=4,
+            sticky=tk.W + tk.E + tk.N + tk.S
+        )
+
+        self.encrypt_btn = tk.Button(
+            root,
+            text="ENKRIPTO",
+            command=self.encrypt_callback,
+            bg="#4e9696",
+            fg="#ffffff",
+            bd=2,
+            relief=tk.FLAT
+        )
+        self.encrypt_btn.grid(
+            padx=(15, 6),
+            pady=8,
+            ipadx=24,
+            ipady=6,
+            row=7,
+            column=0,
+            columnspan=2,
+            sticky=tk.W + tk.E + tk.N + tk.S
+        )
+
+        self.decrypt_btn = tk.Button(
+            root,
+            text="DEKRIPTO",
+            command=self.decrypt_callback,
+            bg="#5e4e96",
+            fg="#ffffff",
+            bd=2,
+            relief=tk.FLAT
+        )
+        self.decrypt_btn.grid(
+            padx=(6, 15),
+            pady=8,
+            ipadx=24,
+            ipady=6,
+            row=7,
+            column=2,
+            columnspan=2,
+            sticky=tk.W + tk.E + tk.N + tk.S
+        )
+
+        self.reset_btn = tk.Button(
+            root,
+            text="RESETO",
+            command=self.reset_callback,
+            bg="#aaaaaa",
+            fg="#ffffff",
+            bd=2,
+            relief=tk.FLAT
+        )
+        self.reset_btn.grid(
+            padx=15,
+            pady=(4, 12),
+            ipadx=24,
+            ipady=6,
+            row=8,
+            column=0,
+            columnspan=4,
+            sticky=tk.W + tk.E + tk.N + tk.S
+        )
+
+        self.status_label = tk.Label(
+            root,
+            textvariable=self._status,
+            bg="#eeeeee",
+            anchor=tk.W,
+            justify=tk.LEFT,
+            relief=tk.FLAT,
+            wraplength=350
+        )
+        self.status_label.grid(
+            padx=12,
+            pady=(0, 12),
+            ipadx=0,
+            ipady=1,
+            row=9,
+            column=0,
+            columnspan=4,
+            sticky=tk.W + tk.E + tk.N + tk.S
+        )
+
+        tk.Grid.columnconfigure(root, 0, weight=1)
+        tk.Grid.columnconfigure(root, 1, weight=1)
+        tk.Grid.columnconfigure(root, 2, weight=1)
+        tk.Grid.columnconfigure(root, 3, weight=1)
+  
