abb8efe8f3771661e90dc44be59d5398c9193e75
kushtrimhaziri <58092838+kushtrimhaziri@users.noreply.github.com>
2021-02-13 15:37:31 +0100

    Creatre

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
 
+import os
+import sys
+import hashlib
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+from Crypto.Cipher import AES
+
+from EncryptionTool import EncryptionTool
+
+
+class Main:
